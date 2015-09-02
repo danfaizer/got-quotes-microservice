7c8f9c24dbb1d07228efac125e3e81482af1eab9
Daniel Beneyto <daniel.beneyto@schibsted.com>
2015-09-02 11:51:07 +0200

    Modified build.sh

--- packer/build.sh
+++ packer/build.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
+set -e
+
 export creator=$(git --no-pager show -s --format='%ae' ${TRAVIS_COMMIT})
 export creation_time=`date +"%Y%m%d%H%M%S"` 
 export appversion="0.0.${TRAVIS_BUILD_ID}" 
 export ec2_source_ami=ami-a10897d6
-packer -machine-readable build packer/build.json | sudo tee output.txt
-tail -2 output.txt | head -2 | awk 'match($0, /ami-.*/) { print substr($0, RSTART, RLENGTH) }' >  ami.txt
+packer -machine-readable build packer/build.json
